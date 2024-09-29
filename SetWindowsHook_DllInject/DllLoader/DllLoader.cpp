696390b5a986aa5700747283a46ee1e9c6f63804
shalunZhou <lunsha498@gmail.com>
2024-09-29 14:03:08 +0800

    SetWindowsHook add commit

---
+++ SetWindowsHook_DllInject/DllLoader/DllLoader.cpp
@@ -0,0 +1,66 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+int wmain(int argc, wchar_t* argv[]) {
+	
+	if (argc < 3) {
+		cout << "Invalid param!" << endl;
+		return 0;
+	}
+
+	const wchar_t* dllPath = argv[1];
+	const wchar_t* windowName = argv[2];
+
+	//获取窗口句柄
+	HWND hWnd = FindWindowW(windowName, NULL);
+	if (hWnd == NULL) {
+		cout << "Could not find target window." << endl;
+		return EXIT_FAILURE;
+	}
+
+	// 获取窗口线程ID
+	DWORD pid = NULL;
+	DWORD tid = GetWindowThreadProcessId(hWnd, &pid);
+	if (tid == NULL) {
+		cout << "Could not get thread ID of the target window." << endl;
+		return EXIT_FAILURE;
+	}
+
+	// 加载dll
+	HMODULE dll = LoadLibraryEx(dllPath, NULL, DONT_RESOLVE_DLL_REFERENCES);
+	if (dll == NULL) {
+		cout << "The DLL could not be found." << endl;
+		return EXIT_FAILURE;
+	}
+
+	// 获取导出函数地址
+	HOOKPROC addr = (HOOKPROC)GetProcAddress(dll, "NextHook"); 
+	if (addr == NULL) {
+		cout << "The function was not found." << endl;
+		return EXIT_FAILURE;
+	}
+
+	// 设置消息钩子
+	HHOOK handle = SetWindowsHookEx(WH_GETMESSAGE, addr, dll, tid); 
+	if (handle == NULL) {
+		cout << "Couldn't set the hook with SetWindowsHookEx." << endl;
+		return EXIT_FAILURE;
+	}
+
+	// 触发钩子
+	PostThreadMessage(tid, WM_NULL, NULL, NULL);
+
+	Sleep(1);
+	
+	//卸载钩子
+	BOOL unhook = UnhookWindowsHookEx(handle);
+	if (unhook == FALSE) {
+		cout << "Could not remove the hook." << endl;
+		return EXIT_FAILURE;
+	}
+
+	cout << "Inject successful!" << endl;
+	return EXIT_SUCCESS;
+}
---
+++ SetWindowsHook_DllInject/hookdll/dllmain.cpp
@@ -0,0 +1,24 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <Windows.h>
+
+
+extern "C" __declspec(dllexport)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
+	switch (ul_reason_for_call) {
+	case DLL_PROCESS_ATTACH:
+		MessageBox(NULL, L"Dll injected", L"Injected", MB_ICONEXCLAMATION | MB_OK);
+		break;
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+
+extern "C" __declspec(dllexport) int NextHook(int code, WPARAM wParam, LPARAM lParam) {
+	return CallNextHookEx(NULL, code, wParam, lParam);
+}
+
